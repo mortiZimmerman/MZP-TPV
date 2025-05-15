b06b7dc96c1dc1a78d19fb3626fc9ef089a0530f
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-05-15 23:00:38 +0200

    chore: update routes for admin user management

--- MZ-TPV/routes/web.php
+++ MZ-TPV/routes/web.php
@@ -1,28 +1,31 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ProfileController;  
 use Illuminate\Support\Facades\Route;
 
+// Ruta pública para la página principal
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->middleware(AdminMiddleware::class)->name('admin.dashboard');
+
+    Route::prefix('admin')->name('admin.')->middleware([AdminMiddleware::class])->group(function () {
+        Route::resource('users', UserController::class);
+    });
 });
-
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware('auth');
-
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
