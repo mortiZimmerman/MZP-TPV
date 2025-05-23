c166a795e9952e54acc332e53c3dea68d5f37a28
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-05-23 20:18:02 +0200

    fix:dashboard button fixed

--- MZ-TPV/routes/web.php
+++ MZ-TPV/routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('/admin/dashboard');
     })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
