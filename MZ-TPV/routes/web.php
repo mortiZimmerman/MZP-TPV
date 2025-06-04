2ea7fb2603d4cac9905643287fd640b515de9cbb
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-05 00:08:31 +0200

    chore: update web routes for products and categories

--- MZ-TPV/routes/web.php
+++ MZ-TPV/routes/web.php
@@ -1,31 +1,41 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\ProfileController;  
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\OtherController;
 
-// Ruta pública para la página principal
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('/admin/dashboard');
+        if (auth()->user()->role !== 'admin') {
+            return redirect()->route('login');
+        }
+        return redirect()->route('admin.dashboard');
     })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/admin/dashboard', function () {
-        return view('admin.dashboard');
-    })->middleware(AdminMiddleware::class)->name('admin.dashboard');
+    Route::prefix('admin')
+         ->middleware(AdminMiddleware::class)
+         ->name('admin.')
+         ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
 
-    Route::prefix('admin')->name('admin.')->middleware([AdminMiddleware::class])->group(function () {
         Route::resource('users', UserController::class);
+        Route::resource('products', ProductController::class);
     });
 });
-Route::get('/admin/other', [OtherController::class, 'index'])->name('admin.someOtherPage');
+
+Route::get('/admin/other', [OtherController::class, 'index'])
+    ->middleware(['auth', AdminMiddleware::class])
+    ->name('admin.someOtherPage');
+
 require __DIR__.'/auth.php';
