c17d4c5e9364efd644e206686dde6b0658475c1b
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-05 00:07:08 +0200

    feat: add header partial for admin pages

--- MZ-TPV/resources/views/admin/partials/header.blade.php
+++ MZ-TPV/resources/views/admin/partials/header.blade.php
@@ -1,8 +1,9 @@
+<link rel="stylesheet" href="{{ asset('css/headerLayout.css') }}">
 <header>
     <div><strong>Sistema TPV para Bares y Restaurantes</strong></div>
     <nav>
         <a href="{{ route('admin.dashboard') }}">Dashboard Admin</a>
-        <a href="#">Manage Products</a>
+        <a href="{{ route('admin.products.index') }}">Manage Products</a>
         <a href="#">Manage Tables</a>
         <a href="#">Manage Orders</a>
     </nav>
