20f38a94a035f86769f72facae66eccca55a462a
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-16 23:23:50 +0200

    feat: add classic index view for tables

--- MZ-TPV/resources/views/tables/index.blade.php
+++ MZ-TPV/resources/views/tables/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@if(auth()->user()->role === 'admin')
+    @include('admin.partials.header')
+@elseif(auth()->user()->role === 'waiter')
+    @include('waiter.partials.header')
+@endif
+
 @section('content')
 <div class="container">
     <h1>Tables</h1>
@@ -7,16 +13,22 @@
     <table class="table table-striped">
         <thead>
             <tr>
-                <th>Client Number</th>
+                <th>ID</th>
+                <th>Number</th>
                 <th>Status</th>
+                <th>X</th>
+                <th>Y</th>
                 <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             @foreach($tables as $table)
             <tr>
+                <td>{{ $table->id }}</td>
                 <td>{{ $table->number }}</td>
                 <td>{{ ucfirst($table->status) }}</td>
+                <td>{{ $table->x ?? '-' }}</td>
+                <td>{{ $table->y ?? '-' }}</td>
                 <td>
                     <a href="{{ route('tables.show', $table) }}" class="btn btn-sm btn-info">View</a>
                     <a href="{{ route('tables.edit', $table) }}" class="btn btn-sm btn-warning">Edit</a>
