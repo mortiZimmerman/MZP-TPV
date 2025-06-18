e9071b0d6ea3d637644d410db49e3ec0d43e0fc1
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-19 01:28:52 +0200

    fix: ensure header styles persist after loader hides

--- MZ-TPV/resources/views/admin/partials/header.blade.php
+++ MZ-TPV/resources/views/admin/partials/header.blade.php
@@ -1,10 +1,13 @@
 <link rel="stylesheet" href="{{ asset('css/headerLayout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminDashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminManagement.css') }}">
+    <link rel="icon" type="image/png" href="https://res.cloudinary.com/dandumvvy/image/upload/v1750285793/logo_negro_hggkgw.png">
 <header>
-    <div><strong>Sistema TPV para Bares y Restaurantes</strong></div>
+    <div><strong> <a href="{{ route('account') }}">My Account</a></strong></div>
     <nav>
         <a href="{{ route('admin.dashboard') }}">Dashboard Admin</a>
         <a href="{{ route('admin.products.index') }}">Manage Products</a>
-        <a href="#">Manage Tables</a>
-        <a href="#">Manage Orders</a>
+        <a href="{{ route('tables.index') }}">Manage Tables</a>
+        <a href="{{ route('admin.orders.index') }}">Manage Orders</a>
     </nav>
 </header>
