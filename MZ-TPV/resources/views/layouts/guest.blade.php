f8270e974638a2bf25b8e7870b05700b8aebd9c1
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-19 01:28:00 +0200

    feat: set custom logo as favicon in guest layout

--- MZ-TPV/resources/views/layouts/guest.blade.php
+++ MZ-TPV/resources/views/layouts/guest.blade.php
@@ -14,17 +14,29 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+ <body class="font-sans text-gray-900 antialiased">
+    
+    <video autoplay muted loop id="bg-video" style="
+        position: fixed;
+        top: 0; left: 0;
+        width: 100vw; height: 100vh;
+        object-fit: cover;
+        z-index: -1;
+    ">
+        <source src="https://res.cloudinary.com/dandumvvy/video/upload/v1749744851/pzmpz/fv6w3sz8nezcwbclm3gl.mp4" type="video/mp4">
+    </video>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+        <div>
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
         </div>
-    </body>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
