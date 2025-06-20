7e7e25ca6fb86ce3444a3671d3b7eeccf75f065f
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-20 11:31:56 +0200

    fix: update user create view

--- MZ-TPV/resources/views/admin/users/create.blade.php
+++ MZ-TPV/resources/views/admin/users/create.blade.php
@@ -8,19 +8,19 @@
         <form action="{{ route('admin.users.store') }}" method="POST" class="styled-form">
             @csrf
 
-            <div class="form-group">
+            <div class="form-group " style="margin-left:-17rem;">
                 <label for="name" ></label>
                 <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control" placeholder="Enter full name" required>
                 @error('name') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="email"></label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control" placeholder="Enter email address" required  >
                 @error('email') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" > 
               
                 <div class="role-buttons">
                     <label><input type="radio" class="formRadio" name="role" value="waiter" {{ old('role') == 'waiter' ? 'checked' : '' }}> Waiter</label>
@@ -29,18 +29,18 @@
                 @error('role') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="password"></label>
                 <input type="password" name="password" id="password" class="form-control" placeholder="Enter password" required >
                 @error('password') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="password_confirmation"></label>
                 <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Confirm password" required >
             </div>
 
-            <div class="form-actions">
+            <div class="form-actions" style="margin-left:-17rem;">
                 
                 <button type="submit" class="submit-btn">Submit</button>
                 <a href="{{ route('admin.users.index') }}" ><button type="button" class="btn-cancel">Cancel</button></a>
