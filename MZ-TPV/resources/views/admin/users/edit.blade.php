62509cb359ffb93dfa327b182d2b13ef0dfa0e14
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-20 11:31:57 +0200

    fix: update user edit view

--- MZ-TPV/resources/views/admin/users/edit.blade.php
+++ MZ-TPV/resources/views/admin/users/edit.blade.php
@@ -9,13 +9,13 @@
             @csrf
             @method('PUT')
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="name"></label>
                 <input type="text" name="name" id="name" value="{{ old('name', $user->name) }}" class="form-control" placeholder="Enter full name" required>
                 @error('name') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="email"></label>
                 <input type="email" name="email" id="email" value="{{ old('email', $user->email) }}" class="form-control" placeholder="Enter email address" required>
                 @error('email') <small class="error">{{ $message }}</small> @enderror
@@ -35,18 +35,18 @@
 </div>
 
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="password"></label>
                 <input type="password" name="password" id="password" class="form-control" placeholder="Enter password">
                 @error('password') <small class="error">{{ $message }}</small> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin-left:-17rem;">
                 <label for="password_confirmation"></label>
                 <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Confirm password">
             </div>
 
-            <div class="form-actions">
+            <div class="form-actions" style="margin-left:-17rem;">
                
                 <button type="submit" class="submit-btn">Update</button>
                 <a href="{{ route('admin.users.index') }}" ><button type="button" class="btn-cancel">Cancel</button></a>
