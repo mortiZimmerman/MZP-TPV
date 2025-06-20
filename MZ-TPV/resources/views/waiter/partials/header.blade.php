6bc1b7aadfcae397a7fd783562b40774fbde397c
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-20 13:25:11 +0200

    fix: update waiter header to use global orders.* routes

--- MZ-TPV/resources/views/waiter/partials/header.blade.php
+++ MZ-TPV/resources/views/waiter/partials/header.blade.php
@@ -1,5 +1,9 @@
+<link rel="stylesheet" href="{{ asset('css/headerLayout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminDashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminManagement.css') }}">
+    <link rel="icon" type="image/png" href="https://res.cloudinary.com/dandumvvy/image/upload/v1750285793/logo_negro_hggkgw.png">
 <header>
-    <div><strong>Sistema TPV para Bares y Restaurantes</strong></div>
+     <div><strong> <a href="{{ route('account') }}">My Account</a></strong></div>
     <nav>
         <a href="{{ route('waiter.dashboard') }}">Waiter Dashboard</a>
         <a href="{{ route('tables.index') }}">Manage Tables</a>
