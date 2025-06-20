73176411b20380d805b4a28d4972eae9f78f5b6c
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-06-20 11:31:54 +0200

    fix: update orders_details migration for correct structure

--- MZ-TPV/database/migrations/2025_05_05_171556_orders_details.php
+++ MZ-TPV/database/migrations/2025_05_05_171556_orders_details.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_details', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->integer('quantity');
-            $table->decimal('unit_price', 8, 2);
-            $table->decimal('subtotal', 10, 2);
-            $table->timestamps();
+           Schema::create('order_details', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('order_id')->constrained()->onDelete('cascade');
+        $table->foreignId('product_id')->constrained()->onDelete('cascade');
+        $table->integer('quantity');
+        $table->decimal('price', 8, 2);
+        $table->timestamps();
         });
         
     }
